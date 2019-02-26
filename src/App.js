522e2bf8e06d2bd5ff2a212843c0699f070cead4
Aymeric Prat <aymeric.prat@gmail.com>
2019-02-26 15:55:16 +0100

    app completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Title from './imports/Title'
+import Scoreboard from './imports/Scoreboard'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Title content='Welcome to my scoreboard app'/>
         </header>
+        <main>
+          <Scoreboard />
+        </main>
       </div>
     );
   }
